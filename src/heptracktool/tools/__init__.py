c75759bb4f6efdca32618b63e738cebaaa9b5ef0
Xiangyang Ju <xju@lbl.gov>
2025-03-07 14:42:56 -0800

    add matplotlib

--- src/heptracktool/tools/__init__.py
+++ src/heptracktool/tools/__init__.py
@@ -1,8 +1,11 @@
 from heptracktool.tools.analyze_track_data import TrackAnalyzer
 from heptracktool.tools.compare_tracking_algs import TrackAlgComparator
-
+from heptracktool.tools.edge_perf import EdgePerformance
+from heptracktool.tools.model_inference import ExaTrkxInference
 
 __all__ = [
     "TrackAnalyzer",
     "TrackAlgComparator",
+    "EdgePerformance",
+    "ExaTrkxInference",
 ]
