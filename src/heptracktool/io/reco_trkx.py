4fb17f4fa3a185b0cb8ded3bcf9e59142c9aa77e
Xiangyang Ju <xju@lbl.gov>
2025-03-10 12:58:50 -0700

    no os path

--- src/heptracktool/io/reco_trkx.py
+++ src/heptracktool/io/reco_trkx.py
@@ -1,7 +1,6 @@
-import os
 import re
-import glob
 from typing import Any
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -14,13 +13,12 @@ class RecoTrkxReader:
     def __init__(self, reco_track_path: str):
         self.path = reco_track_path
 
-        all_files = glob.glob(os.path.join(reco_track_path, "*.npz"))
+        all_files = Path(reco_track_path).glob("*.npz")
         self.nevts = len(all_files)
-        self.all_evtids = [int(os.path.basename(x)[:-4]) for x in all_files]
+        self.all_evtids = [int(Path(x).name[:-4]) for x in all_files]
 
     def __call__(self, evtid: int, *args: Any, **kwds: Any) -> Any:
-        """Return reconstructed track candidates"""
-        tracks_fname = os.path.join(self.path, f"{evtid}.npz")
+        tracks_fname = Path(self.path) / f"{evtid}.npz"
         arrays = np.load(tracks_fname)
         predicts = arrays["predicts"]
         if predicts.shape[1] == 2:
@@ -39,24 +37,21 @@ class ACTSTrkxReader:
     """Read reconstructed tracks dumped from the CKF method in ACTS"""
 
     def __init__(self, reco_track_path: str):
-        self.path = reco_track_path
+        self.path = Path(reco_track_path)
         self.filename = "event{:09}-CKFtracks.csv"
 
-        all_files = glob.glob(os.path.join(self.path, "event*-CKFtracks.csv"))
+        all_files = Path(self.path).glob("event*-CKFtracks.csv")
         self.nevts = len(all_files)
 
         pattern = "event([0-9]*)-CKFtracks.csv"
-        self.all_evtids = sorted(
-            [
-                int(re.search(pattern, os.path.basename(x)).group(1).strip())
-                for x in all_files
-            ]
-        )
-        print(f"Total {self.nevts} events in directory: {self.path}")
+        self.all_evtids = [
+            int(re.search(pattern, Path(x).name).group(1).strip()) for x in all_files
+        ]
+        print(f"Total {self.nevts} events in directory: {self.path!s}")
 
     def __call__(self, evtid: int, *args: Any, **kwds: Any) -> Any:
         """Return reconstructed track candidates"""
-        tracks_fname = os.path.join(self.path, self.filename.format(evtid))
+        tracks_fname = self.path / self.filename.format(evtid)
         submission = pd.read_csv(tracks_fname)
         submission.rename(columns={"measurement_id": "hit_id"}, inplace=True)
         return submission
