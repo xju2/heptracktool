8d458c307911326b54f14e30abf26ed49e59f451
Xiangyang Ju <xju@lbl.gov>
2026-02-02 23:49:00 -0800

    add new data

--- scripts/run_ckf.sh
+++ scripts/run_ckf.sh
@@ -2,7 +2,8 @@
 
 # shifter --image=beojan/mpicuda9-2:latest --module=cvmfs,gpu ../scripts/run_ckf.sh
 
-PT_CUTS=(200 400 500 700 900 1000)
+# PT_CUTS=(200 400 500 700 900 1000)
+PT_CUTS=(300)
 rdo=/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000047.pool.root.1
 MAX_EVENTS=10
 
